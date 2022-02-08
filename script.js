1a24ef766c7d9e6f988935f153a9ee65a78a24b5
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-08 23:38:10 +0600

    adding reset functionality to the app

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
@@ -30,3 +31,10 @@ p2Button.addEventListener('click', () => {
   p2Display.textContent = p2Score;
 });
 
+resetButton.addEventListener('click', () => {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+});
