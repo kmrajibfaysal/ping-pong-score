65b3b9903150f9aac293184264fd5ff0975dc0e3
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-08 22:46:09 +0600

    adding game over funtion to the app

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
-  p1Score += 1;
+  if (!isGameOver) {
+    p1Score += 1;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
+  }
+
   p1Display.textContent = p1Score;
 });
+
+p2Button.addEventListener('click', () => {
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
+  }
+
+  p2Display.textContent = p2Score;
+});
+
