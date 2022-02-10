c40f1f69119662f6a9db11465be160b962dc3f54
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-10 17:07:44 +0600

    removing duplicate code and made it dry

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const resetButton = document.querySelector('#reset');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const winningScoreSelect = document.querySelector('#playto');
+const p1 = {
+  score: 0,
+  button: document.querySelector('#p1Button'),
+  display: document.querySelector('#p1Display'),
+};
+const p2 = {
+  score: 0,
+  button: document.querySelector('#p2Button'),
+  display: document.querySelector('#p2Display'),
+};
 
-let p1Score = 0;
-let p2Score = 0;
-let winningScore = 5;
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', () => {
+function updateScores(player, opponent) {
   if (!isGameOver) {
-    p1Score += 1;
-    if (p1Score === winningScore) {
+    player.score += 1;
+    if (player.score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add('winner');
-      p2Display.classList.add('loser');
+      player.display.classList.add('winner');
+      opponent.display.classList.add('loser');
+      player.disabled = true;
+      opponent.disabled = true;
     }
+    player.display.textContent = player.score;
   }
+}
 
-  p1Display.textContent = p1Score;
+p1.button.addEventListener('click', () => {
+  updateScores(p1, p2);
 });
 
-p2Button.addEventListener('click', () => {
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p2Display.classList.add('winner');
-      p1Display.classList.add('loser');
-    }
-  }
-  p2Display.textContent = p2Score;
+p2.button.addEventListener('click', () => {
+  updateScores(p2, p1);
 });
-
 
 function reset() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = 0;
-  p2Display.textContent = 0;
-  p1Display.classList.remove('winner', 'loser');
-  p2Display.classList.remove('winner', 'loser');
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove('winner', 'loser');
+    p.button.disabled = false;
+  }
 }
 
 winningScoreSelect.addEventListener('change', function () {
   winningScore = parseInt(this.value);
   reset();
-  
 });
-
-
 
 resetButton.addEventListener('click', reset);
